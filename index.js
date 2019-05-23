df2d61114b7f3d0f2c717b34bd9f117415254611
Omuzi <49815401+Omuzi@users.noreply.github.com>
2019-05-23 13:46:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function play(connection, message) {
 client.on("ready", () => {
 
   console.log("Je suis prêt !");
-  client.user.setActivity('les mises à jour à venir ', { type: 'WATCHING' })
+  client.user.setActivity('les mises à jour à venir ', { type: 'Streaming' })
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '*help'}`))
   .catch(console.error);
 
